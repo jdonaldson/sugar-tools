422d5cc2a27fd6de4c00a94e28ea999291c52759
jdonaldson <jdonaldson@gmail.com>
2010-09-25 22:17:17 -0700

    modified demo slightly.

--- demo/MacroFastDemo.hx
+++ demo/MacroFastDemo.hx
@@ -4,5 +4,9 @@ class MacroFastDemo {
 		var y = MacroFast.construct('assets/simple.xml');
 		// generate a --display completion below...
 		trace(y.node.CD.node.ARTIST);
+		
+		//edit things as normal...
+		y.node.CD.node.ARTIST = 'Bruce Springsteen';
+		trace(y.node.CD.node.ARTIST);
 	}
 }
