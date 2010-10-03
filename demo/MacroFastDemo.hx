02e7a6cc5e7e3ac121a5ecb29a9356fd43b7e638
Justin Donaldson <jjdonald@Justin-Donaldsons-MacBook-Pro.local>
2010-10-03 00:42:26 -0700

    modified demo slightly

--- demo/MacroFastDemo.hx
+++ demo/MacroFastDemo.hx
@@ -2,8 +2,10 @@ import org.sugar.xml.MacroFast;
 class MacroFastDemo {
 	public static function main(){
 		var y = MacroFast.parseFile('assets/simple.xml');
-		// generate a --display completion below...
+		// generate --display completions below...
 		trace(y.node.cd.node.artist.innerData);
 		trace(y.nodes.cd[4].node.artist.innerData);
+		trace(y.node.cd.elements[5].innerData);
 	}
+
 }
